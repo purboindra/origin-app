35529549271fc8bc9009182f5f1169663fbe13d6
purboindra <purboindra@gmail.com>
2025-04-14 12:50:07 +0700

    feat: ThumbnailImageForm

---
+++ components/shared/thumbnail-image-form.tsx
@@ -0,0 +1,88 @@
+"use client"
+
+import { createProductSchema } from "@/lib/validation"
+import React from "react";
+import { useFormContext, useWatch } from "react-hook-form"
+import { z } from "zod"
+import { FormControl, FormField, FormItem } from "../ui/form";
+import Image from "next/image";
+import { Input } from "../ui/input";
+import { Camera } from "lucide-react";
+
+export default function ThumbnailImageForm(){
+    const form = useFormContext<z.infer<typeof createProductSchema>>();
+
+  const image = useWatch({
+    control: form.control,
+    name: "thumbnail_image",
+    defaultValue: undefined,
+  });
+
+
+  React.useEffect(() => {
+    if (image instanceof File) {
+      const objectUrl = URL.createObjectURL(image);
+      return () => URL.revokeObjectURL(objectUrl);
+    }
+  }, [image]);
+
+    return (
+        <FormField
+        control={form.control}
+        name="thumbnail_image"
+        render={({ field: { onChange } }) => (
+          <FormItem>
+            <FormControl>
+              <div className="relative w-96 h-96">
+                {image ? (
+                  <div>
+                    <Image
+                      src={
+                        image instanceof File
+                          ? URL.createObjectURL(image)
+                          : typeof image === "string"
+                          ? image
+                          : ""
+                      }
+                      alt="Thumbnail Image"
+                      fill
+                      className="object-cover rounded-md"
+                    />
+                    <label htmlFor="thumbnail_image" className="absolute inset-0 z-10 cursor-pointer" />
+                    <Input
+                      type="file"
+                      id="thumbnail_image"
+                      className="hidden"
+                      onChange={(e) => {
+                        if (e.target.files?.[0]) {
+                          onChange(e.target.files[0]);
+                        }
+                      }}
+                    />
+                  </div>
+                ) : (
+                  <div className="flex w-full h-full items-center justify-center bg-gray-200 rounded-md">
+                    <label htmlFor="thumbnail_image" className="cursor-pointer">
+                      <Camera size={140} className="text-blue-800" />
+                      <Input
+                      type="file"
+                      id="thumbnail_image"
+                      className="hidden"
+                      onChange={(e) => {
+                        if (e.target.files?.[0]) {
+                          onChange(e.target.files[0]);
+                        }
+                      }}
+                    />
+                    </label>
+                   
+                  </div>
+                )}
+              </div>
+            </FormControl>
+          </FormItem>
+        )}
+      />
+    )
+
+}
--- components/shared/upload-image-component.tsx
+++ components/shared/upload-image-component.tsx
@@ -1,92 +1,18 @@
 "use client";
 
-import { Camera, Plus } from "lucide-react";
 import React from "react";
-import Image from "next/image";
-import { useFormContext, useWatch } from "react-hook-form";
-import { z } from "zod";
-import { createProductSchema } from "@/lib/validation";
-import { FormControl, FormField, FormItem } from "../ui/form";
+
+import ThumbnailImageForm from "./thumbnail-image-form";
 import VariantImagesForm from "./variant-images-form";
-import { Input } from "../ui/input";
 
 export default function UploadImageComponent() {
-  const form = useFormContext<z.infer<typeof createProductSchema>>();
-
-  const image = useWatch({
-    control: form.control,
-    name: "thumbnail_image",
-    defaultValue: undefined,
-  });
-
-
-  React.useEffect(() => {
-    if (image instanceof File) {
-      const objectUrl = URL.createObjectURL(image);
-      return () => URL.revokeObjectURL(objectUrl);
-    }
-  }, [image]);
+ 
 
   return (
     <div className="flex flex-1 flex-col">
       <h1 className="text-4xl font-semibold text-blue-800">Upload Gambar</h1>
       <div className="mt-8 flex flex-col gap-6">
-      <FormField
-  control={form.control}
-  name="thumbnail_image"
-  render={({ field: { onChange } }) => (
-    <FormItem>
-      <FormControl>
-        <div className="relative w-96 h-96">
-          {image ? (
-            <div>
-              <Image
-                src={
-                  image instanceof File
-                    ? URL.createObjectURL(image)
-                    : typeof image === "string"
-                    ? image
-                    : ""
-                }
-                alt="Thumbnail Image"
-                fill
-                className="object-cover rounded-md"
-              />
-              <label htmlFor="thumbnail_image" className="absolute inset-0 z-10 cursor-pointer" />
-              <Input
-                type="file"
-                id="thumbnail_image"
-                className="hidden"
-                onChange={(e) => {
-                  if (e.target.files?.[0]) {
-                    onChange(e.target.files[0]);
-                  }
-                }}
-              />
-            </div>
-          ) : (
-            <div className="flex w-full h-full items-center justify-center bg-gray-200 rounded-md">
-              <label htmlFor="thumbnail_image" className="cursor-pointer">
-                <Camera size={140} className="text-blue-800" />
-                <Input
-                type="file"
-                id="thumbnail_image"
-                className="hidden"
-                onChange={(e) => {
-                  if (e.target.files?.[0]) {
-                    onChange(e.target.files[0]);
-                  }
-                }}
-              />
-              </label>
-             
-            </div>
-          )}
-        </div>
-      </FormControl>
-    </FormItem>
-  )}
-/>
+    <ThumbnailImageForm/>
 
 {/* VARIANT IMAGES */}
 
