1c6c5bab27e859a9534c14acf41ac68a984a75eb
purboyndra <purboindra@gmail.com>
2025-04-16 20:12:59 +0700

    feat: ProductInterface

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,17 @@
+import { ObjectId } from "mongodb";
+
 export interface CategoryInterface {
   name: string;
+}
+
+export interface ProductInterface {
+  _id: ObjectId;
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+  thumbnail_image: string;
+  images: string[];
+  category: CategoryInterface;
 }
