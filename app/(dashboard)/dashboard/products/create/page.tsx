46b09e30132dc63c02329d8bee83ed9f2a3b0338
purboindra <purboindra@gmail.com>
2025-04-16 12:31:30 +0700

    fix: remove unused fetchcategories

--- app/(dashboard)/dashboard/products/create/page.tsx
+++ app/(dashboard)/dashboard/products/create/page.tsx
@@ -2,10 +2,6 @@ import { fetchCategories } from "@/action/category.action";
 import CreateProductsContent from "@/components/shared/create-products-content";
 
 export default async function CreateProductsPage() {
-  const categories = await fetchCategories();
-
-  console.log("categories", categories);
-
   return (
     <section className="w-full min-h-screen px-8 py-8">
       <CreateProductsContent />
