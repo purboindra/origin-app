d4bc890cd726674e6c1cac4de1e0ba430178be0c
purboyndra <purboindra@gmail.com>
2025-04-17 19:11:38 +0700

    fix: layout strategy

--- app/(dashboard)/dashboard/(auth)/layout.tsx
+++ app/(dashboard)/dashboard/(auth)/layout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <section>{children}</section>;
+  return <section className="w-full min-h-screen">{children}</section>;
 }
